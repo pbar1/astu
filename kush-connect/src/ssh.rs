33f85e5c5a00395b412a98f14f8b6bb3f3caee36
Pierce Bartine <piercebartine@gmail.com>
2024-08-31 18:01:42 -0700

    agent auth

--- kush-connect/src/ssh.rs
+++ kush-connect/src/ssh.rs
@@ -2,8 +2,11 @@ use std::path::Path;
 use std::sync::Arc;
 use std::time::Duration;
 
+use anyhow::bail;
 use anyhow::Context;
 use tokio::io::AsyncWriteExt;
+use tracing::debug;
+use tracing::error;
 
 pub struct SshClient {
     session: russh::client::Handle<SshClientHandler>,
@@ -25,7 +28,7 @@ impl SshClient {
         Ok(Self { session })
     }
 
-    async fn auth(
+    async fn auth_key(
         &mut self,
         key_path: impl AsRef<Path>,
         user: impl Into<String>,
@@ -42,6 +45,24 @@ impl SshClient {
         }
 
         Ok(())
+    }
+
+    async fn auth_agent(&mut self, user: &str) -> anyhow::Result<()> {
+        let mut agent = russh::keys::agent::client::AgentClient::connect_env().await?;
+        let mut result: Result<bool, _>;
+        let identities = agent.request_identities().await?;
+
+        for key in identities {
+            let fingerprint = key.fingerprint();
+            (agent, result) = self.session.authenticate_future(user, key, agent).await;
+            match result {
+                Ok(true) => return Ok(()),
+                Ok(false) => debug!(%user, key = %fingerprint, "ssh agent auth denied"),
+                Err(error) => error!(?error, "ssh agent auth failed"),
+            }
+        }
+
+        bail!("unable to authenticate with ssh agent");
     }
 
     async fn exec(&mut self, command: &str) -> anyhow::Result<ExecOutput> {
@@ -134,11 +155,16 @@ mod tests {
     #[tokio::test]
     async fn works() {
         let tcp_factory = ReuseportTcpFactory::try_new().unwrap();
-        let addr = "127.0.0.1:2222".to_socket_addrs().unwrap().next().unwrap();
+        let addr = "tec.lan:22".to_socket_addrs().unwrap().next().unwrap();
         let stream = tcp_factory
             .connect_timeout_async(&addr, Duration::from_secs(5))
             .await
             .unwrap();
-        let _ssh_client = SshClient::connect(stream).await.unwrap();
+        let mut ssh_client = SshClient::connect(stream).await.unwrap();
+        ssh_client.auth_agent("nixos").await.unwrap();
+        let output = ssh_client.exec("uname -a").await.unwrap();
+        assert_eq!(output.exit_status, 0);
+        let stdout = String::from_utf8_lossy(&output.stdout);
+        assert!(stdout.contains("GNU/Linux"));
     }
 }
