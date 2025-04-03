767d83666b2340e0ce5a2d4a84bf1886ede35211
Pierce Bartine <piercebartine@gmail.com>
2025-04-02 22:05:48 -0700

    feat(docs): Better social card

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -24,10 +24,7 @@ function HomepageHeader() {
 export default function Home(): ReactNode {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
-    >
+    <Layout title="Hello friend" description="Remote execution multitool">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
