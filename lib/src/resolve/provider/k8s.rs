72f5e027a33919cfd9df7dae058244506dc3839d
Pierce Bartine <piercebartine@gmail.com>
2025-04-25 14:22:58 -0700

    chore: Suppress clippy lint in K8s wip

--- lib/src/resolve/provider/k8s.rs
+++ lib/src/resolve/provider/k8s.rs
@@ -2,15 +2,12 @@ use std::str::FromStr;
 
 use anyhow::Context;
 use anyhow::Result;
-use async_stream::try_stream;
 use futures::stream::BoxStream;
-use futures::StreamExt;
 use k8s_openapi::api::core::v1::Pod;
 use kube::api::ListParams;
 use kube::api::PartialObjectMeta;
 use kube::Api;
 use kube::Client;
-use kube::Resource;
 use kube::ResourceExt;
 
 use crate::resolve::Resolve;
@@ -21,23 +18,23 @@ use crate::resolve::Target;
 pub struct K8sResolver {}
 
 impl Resolve for K8sResolver {
-    fn resolve_fallible(&self, target: Target) -> BoxStream<Result<Target>> {
+    fn resolve_fallible(&self, _target: Target) -> BoxStream<Result<Target>> {
         todo!()
     }
 }
 
-async fn resolve_namespace(namespace: &str) -> Result<Vec<Target>> {
+async fn _resolve_namespace(_namespace: &str) -> Result<Vec<Target>> {
     let client = Client::try_default().await?;
     let pods: Api<Pod> = Api::default_namespaced(client);
 
     pods.list_metadata(&ListParams::default())
         .await?
         .iter()
-        .map(target_from_pod_metadata)
+        .map(_target_from_pod_metadata)
         .collect()
 }
 
-fn target_from_pod_metadata(pod: &PartialObjectMeta<Pod>) -> Result<Target> {
+fn _target_from_pod_metadata(pod: &PartialObjectMeta<Pod>) -> Result<Target> {
     let mut s = "k8s:".to_owned();
     if let Some(namespace) = pod.namespace() {
         s.push_str(&namespace);
